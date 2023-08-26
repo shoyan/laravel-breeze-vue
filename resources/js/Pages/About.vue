8e6a385c48c00cad803150e4887bfbfc2ef9b3e4
Shohei Yamasaki <yamasaki0406@gmail.com>
2023-08-26 12:11:23 +0900

    install breeze

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    message: String,
+});
+</script>
+
+<template>
+<h1 id="message">{{ message }}</h1>
+</template>
+
+<style>
+#message {
+    font-size: 24px;
+    margin: 20px;
+}
+</style>
