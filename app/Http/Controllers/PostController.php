1ad13d927f21ca8d87691ecc4525e18d52d77763
Shohei Yamasaki <yamasaki0406@gmail.com>
2023-10-03 21:59:19 +0900

    ポリシーのサンプルコード

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,12 +22,9 @@ class PostController extends Controller
     public function update(Request $request, Post $post)
     {
         // ポリシーエラーの場合は例外を発生させる
-        Gate::authorize('update', $post);
-
-        // $response = Gate::inspect('update', $post);
-        // if (! $response->allowed()) {
-        //      abort(403);
-        // }
+        if (! Gate::allows('update-post', $post)) {
+            abort(403);
+        }
 
         // 投稿を更新…
         $post->title = "更新されている";
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,6 +30,9 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
+        if ($request->user()->cannot('update', $request->user())) {
+            abort(403);
+        }
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
